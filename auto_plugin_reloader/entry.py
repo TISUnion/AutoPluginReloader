6c01c22f4efa0d25aaf622d0df12505aa8bf847a
Fallen_Breath <yqx791125833@gmail.com>
2021-08-28 20:41:53 +0800

    show neat status after help message

--- auto_plugin_reloader/entry.py
+++ auto_plugin_reloader/entry.py
@@ -8,15 +8,17 @@ PREFIX = '!!apr'
 
 def show_help(source: CommandSource):
     source.reply(tr('help_message', name=metadata.name, version=metadata.version, prefix=PREFIX))
+    show_status(source, True)
 
 
-def show_status(source: CommandSource):
+def show_status(source: CommandSource, full: bool):
     source.reply(tr('status.0', RText(common.reloader.is_running(), RColor.green if common.reloader.is_running() else RColor.red)))
     if common.reloader.is_running():
         source.reply(tr('status.1', common.reloader.get_pretty_next_detection_time()))
-    source.reply(tr('status.2', len(common.config.blacklist)))
-    for item in common.config.blacklist:
-        source.reply('- {}'.format(item))
+    if full:
+        source.reply(tr('status.2', len(common.config.blacklist)))
+        for item in common.config.blacklist:
+            source.reply('- {}'.format(item))
 
 
 def set_enable(source: CommandSource, value: bool):
@@ -38,7 +40,7 @@ def register(server: PluginServerInterface):
         Literal(PREFIX).
         requires(lambda src: src.has_permission(common.config.permission), lambda: tr('permission_denied')).
         runs(show_help).
-        then(Literal('status').runs(show_status)).
+        then(Literal('status').runs(lambda src: show_status(src, False))).
         then(Literal('enable').runs(lambda src: set_enable(src, True))).
         then(Literal('disable').runs(lambda src: set_enable(src, False))).
         then(Literal('set_interval').then(Integer('interval_sec').at_min(1).runs(lambda src, ctx: set_interval(src, ctx['interval_sec']))))
